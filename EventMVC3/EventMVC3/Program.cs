ac15c54e6f12c97b4ec372420b3447e59ce79831
Maram-44 <maramaljoohy@gmail.com>
2025-04-29 10:48:39 +0300

    solve problem of umalqura calender

--- EventMVC3/EventMVC3/Program.cs
+++ EventMVC3/EventMVC3/Program.cs
@@ -1,15 +1,31 @@
 using EventMVC3.Data;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// ÅÚÏÇÏ ËŞÇİÉ ÊÏÚã ÇáÊŞæíã ÇáãíáÇÏí (ãËá en-US Ãæ ar-EG)
+var cultureInfo = new CultureInfo("en-US"); // Ãæ "ar-EG" ÅĞÇ ÃÑÏÊ ÚÑÖ ÚÑÈí áßä ÈÇáÊŞæíã ÇáãíáÇÏí
+
+// ÊÚííä ÇáËŞÇİÉ ßÅÚÏÇÏ ÇİÊÑÇÖí
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(cultureInfo),
+    SupportedCultures = new List<CultureInfo> { cultureInfo },
+    SupportedUICultures = new List<CultureInfo> { cultureInfo }
+};
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<EventContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("myConnection")));
 
-builder.Services.AddDbContext<EventContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("myConnection")));
 var app = builder.Build();
+
+// ÊİÚíá ÇáÅÚÏÇÏÇÊ ÇáãÍáíÉ
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
